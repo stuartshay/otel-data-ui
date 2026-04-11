dd0760b7f21319b3b48d4ec24cf7ea491330e8f2
Stuart Shay <sshay@yahoo.com>
2026-04-11 19:57:57 -0400

    fix: remove duplicate date_from/date_to properties in GarminPage test

--- src/pages/GarminPage.test.tsx
+++ src/pages/GarminPage.test.tsx
@@ -49,8 +49,6 @@ describe('GarminPage', () => {
           sport?: string
           date_from?: string
           date_to?: string
-          date_from?: string
-          date_to?: string
           order: string
           sort: string
         }
@@ -108,8 +106,6 @@ describe('GarminPage', () => {
     expect(garminHooks.useGarminActivitiesQuery).toHaveBeenLastCalledWith({
       variables: {
         limit: 25,
-        date_from: undefined,
-        date_to: undefined,
         offset: 25,
         sport: 'cycling',
         date_from: undefined,
